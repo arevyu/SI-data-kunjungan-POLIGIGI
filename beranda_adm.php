90f3df554b498b45aef3b9999f14256a416adff6
arevyu <77561382+arevyu@users.noreply.github.com>
2021-01-17 11:29:20 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-success">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-danger">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Poli Gigi Kisaran</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/poli.jpg"></img></p>
+                          <p class="label-title" align="center"><strong>POLI GIGI KISARAN</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Poli Gigi Kisaran.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data pengunjung, pengelolaan data dokter, <br>
+                            dan juga pengelolaan laporan pengunjung serta dokter.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data pengunjung dan data dokter.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
